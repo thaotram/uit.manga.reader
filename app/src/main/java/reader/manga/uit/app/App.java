837ae21a561d5d7f085dbc90f5111995934c504f
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-12-14 22:30:27 +0700

    Create App class

---
+++ app/src/main/java/reader/manga/uit/app/App.java
@@ -0,0 +1,13 @@
+package reader.manga.uit.app;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
