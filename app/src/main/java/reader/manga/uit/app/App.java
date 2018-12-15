df16c9131070c97cf7081ad87e151d2acca599cd
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-12-15 10:06:20 +0700

    Add Const.setResource

--- app/src/main/java/reader/manga/uit/app/App.java
+++ app/src/main/java/reader/manga/uit/app/App.java
@@ -3,11 +3,13 @@ package reader.manga.uit.app;
 import android.app.Application;
 
 import io.realm.Realm;
+import model.Const;
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Const.setResource(getResources());
         Realm.init(this);
     }
 }
