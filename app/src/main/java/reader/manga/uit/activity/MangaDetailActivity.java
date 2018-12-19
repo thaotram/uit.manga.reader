bea339a9c22bc44656ceee3582c96e20c56b99c5
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-12-19 19:36:45 +0700

    Update MangaDetail

--- app/src/main/java/reader/manga/uit/activity/MangaDetailActivity.java
+++ app/src/main/java/reader/manga/uit/activity/MangaDetailActivity.java
@@ -2,15 +2,14 @@ package reader.manga.uit.activity;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.databinding.ObservableBoolean;
 import android.os.Bundle;
 import android.view.View;
 
-import java.util.Observable;
-
 import model.model.Manga;
 import reader.manga.uit.R;
+import reader.manga.uit.apollo.FetchData;
 import reader.manga.uit.databinding.ActivityMangaDetailBinding;
+import ui.Swipe;
 
 public class MangaDetailActivity extends AppActivity {
     public static final int LAYOUT = R.layout.activity_manga_detail;
@@ -30,6 +29,11 @@ public class MangaDetailActivity extends AppActivity {
         int mangaId = intent.getIntExtra(MANGA_ID, -1);
         if (mangaId == -1) finish();
         manga = realm.where(Manga.class).equalTo("id", mangaId).findFirst();
+        if (manga == null) {
+            finish();
+            return;
+        }
+        FetchData.FetchManga(this, realm, manga.getId());
     }
 
     private void initializeDataBinding() {
@@ -41,7 +45,8 @@ public class MangaDetailActivity extends AppActivity {
         finish();
     }
 
-    public class State extends Observable {
-        public final ObservableBoolean isOpenDrawer = new ObservableBoolean(false);
+    @SuppressWarnings("unused")
+    public void onRefresh(Swipe swipe) {
+        FetchData.FetchManga(this, realm, manga.getId(), () -> swipe.setRefreshing(false));
     }
 }
