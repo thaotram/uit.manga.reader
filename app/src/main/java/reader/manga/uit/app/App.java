b0c72244d1d8882dd8ec808492aa30a470be9cf9
Lê Thị Thảo Trâm <sawa.kisei@gmail.com>
2018-12-20 10:35:21 +0700

    Update chapter detail

--- app/src/main/java/reader/manga/uit/app/App.java
+++ app/src/main/java/reader/manga/uit/app/App.java
@@ -2,6 +2,8 @@ package reader.manga.uit.app;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 import io.realm.Realm;
 import model.Const;
 import reader.manga.uit.apollo.ApolloClientHelper;
@@ -12,6 +14,7 @@ public class App extends Application {
         super.onCreate();
         Const.setResource(getResources());
         Realm.init(this);
+        Fresco.initialize(this);
         ApolloClientHelper.Initialize(this);
     }
 }
